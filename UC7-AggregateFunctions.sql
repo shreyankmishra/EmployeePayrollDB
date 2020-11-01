bf2ccc38ec4cd996c18038ce4df6dcbaeb16be99
shreyankmishra <mishra.shreyank@gmail.com>
2020-11-01 19:06:27 +0530

    Add: Using various Aggregate functions

---
+++ UC7-AggregateFunctions.sql
@@ -0,0 +1,5 @@
+select SUM(salary) from employee_payroll group by gender
+select AVG(salary) from employee_payroll group by gender
+select MIN(salary) from employee_payroll group by gender
+select MAX(salary) from employee_payroll group by gender
+select COUNT(salary) from employee_payroll group by gender
